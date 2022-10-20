02293ae7db32188bbf462088371b3d5a50aa6dca
okatsn <okatsn@gmail.com>
2022-10-20 15:42:46 +0800

    Register HypertextTools; some comments.

--- add_local_pkg_to_registry.jl
+++ add_local_pkg_to_registry.jl
@@ -1,8 +1,19 @@
 # Reference
 # https://blog.devgenius.io/the-most-underrated-feature-of-the-julia-programming-language-the-package-manager-652065f45a3a
-
 # Noted that the Project.toml and Registry.toml is not allowed in a registry folder; add LocalRegistry in @v1.6.
 
+
+using LocalRegistry
+
+## Local paths to packages
+# this local path dependent approach should be abandon some day.
+try
+    cd("OkRegistry") 
+    using Pkg
+    Pkg.activate(".")
+catch
+    @warn "I think you are already in the OkRegistry folder and presumed that the environment is activated as OkRegistry."
+end
 dirmain(args...) = joinpath(dirname(pwd()), args...) # i.e. raw"D:\GoogleDrive\1Programming\julia"
 dir_myregistry = dirmain("OkRegistry")
 
@@ -14,17 +25,16 @@ mypkgnames = [
 ]
 
 localpkgpaths = dirmain.(mypkgnames .* ".jl")
+dpath = Dict(mypkgnames .=> localpkgpaths)
 
-# Create a local registry (only once)
-using LocalRegistry
-create_registry(dir_myregistry, # name
-    "https://github.com/okatsn/OkRegistry.git", # repository url that already exists
-    ; description="A julia local registry", # optional
-    push=true
-)
 
-# Add local package to the registry created before
+# Update the registry of one package 
+register(dpath["HypertextTools"], registry = dir_myregistry, push=true) 
 
+
+
+
+## Add all local package to the registry created before
 
 for pkgpath in localpkgpaths
     register(
@@ -33,6 +43,21 @@ for pkgpath in localpkgpaths
         push=true # optional
     )
 end
+
+
+
+
+
+
+
+
+## Create a local registry (only once)
+create_registry(dir_myregistry, # name
+    "https://github.com/okatsn/OkRegistry.git", # repository url that already exists
+    ; description="A julia local registry", # optional
+    push=true
+)
+
 
 # In cases you use `Pkg.develop("SomeNewPkg")` that was linked to the path such as "C:/Users//.julia/dev/SomeNewPkg", register the package like this (the following code don't work):
 
