e8b616d9827c6e79f7bbef58090ca85ec7fe07c4
okatsn <okatsn@gmail.com>
2022-12-16 11:22:58 +0800

    Informative msg for script

--- add_local_pkg_to_registry.jl
+++ add_local_pkg_to_registry.jl
@@ -1,3 +1,4 @@
+# # This script works as pwd() is the directory to the local repository of OkRegistry (where you have Project.toml, Registry.toml, .gitignore, and etc..)
 # Reference
 # https://blog.devgenius.io/the-most-underrated-feature-of-the-julia-programming-language-the-package-manager-652065f45a3a
 # Noted that the Project.toml and Registry.toml is not allowed in a registry folder; add LocalRegistry in @v1.6.
